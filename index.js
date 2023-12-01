f9443f4d94ece7e4cb425a794fd1e02abbe73d1d
Shagomir <m.j.knudson@gmail.com>
2023-11-30 19:48:32 -0600

    adding employee functions

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
+//create DB connections
 const db = mysql.createConnection(
   {
     host: "localhost",
@@ -11,6 +12,7 @@ const db = mysql.createConnection(
   //   console.log(`Connected to the employee_db database.`)
 );
 
+//function to grab a list of departments from DB
 const getDepartments = function () {
   let deptList = [];
   db.query(`SELECT dept_name FROM department`, (err, result) => {
@@ -25,6 +27,38 @@ const getDepartments = function () {
   return deptList;
 };
 
+//function to grab a list of role titles from DB
+const getRoles = function () {
+  let roleList = [];
+  db.query(`SELECT title FROM role`, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    result.forEach((element) => {
+      roleList.push(element.title);
+    });
+    // console.log(roleList);
+  });
+  return roleList;
+};
+
+//function to grab a list of employees from DB
+const getEmployees = function () {
+  let managerList = [];
+  db.query(`SELECT first_name, last_name FROM employee`, (err, result) => {
+    if (err) {
+      console.log(err);
+    }
+    result.forEach((element) => {
+      let managerName = element.first_name + " " + element.last_name;
+      managerList.push(managerName);
+    });
+    // console.log(managerList);
+  });
+  return managerList;
+};
+
+//view all depts
 function viewDepartments() {
   db.query(`SELECT id, dept_name FROM department`, (err, result) => {
     if (err) {
@@ -34,6 +68,8 @@ function viewDepartments() {
     repeatCheck();
   });
 }
+
+//view all roles
 function viewRoles() {
   db.query(
     `SELECT role.id, title, salary, dept_name, department_id
@@ -48,6 +84,8 @@ function viewRoles() {
     }
   );
 }
+
+//view all employees
 function viewEmployees() {
   db.query(
     `SELECT employee.id, first_name, last_name, title, manager_id
@@ -61,6 +99,8 @@ function viewEmployees() {
     }
   );
 }
+
+//add a department
 function addDepartment() {
   inquirer
     .prompt({
@@ -84,6 +124,8 @@ function addDepartment() {
       );
     });
 }
+
+//add a role. automatically updates the list of departments to choose.
 function addRole() {
   const depts = getDepartments();
 
@@ -138,10 +180,144 @@ function addRole() {
     });
   }, 100);
 }
-function addEmployee() {}
-function updateEmployee() {}
-function employeeDetails() {}
 
+//add an employee. Auto updates the available titles and managers
+function addEmployee() {
+  const roles = getRoles();
+  const manager = getEmployees();
+
+  setTimeout(() => {
+    const questions = [
+      {
+        type: "input",
+        name: "firstName",
+        message: "Please enter the employee's First name",
+      },
+      {
+        type: "input",
+        name: "lastName",
+        message: "Please enter the employee's Last name",
+      },
+      {
+        type: "list",
+        name: "roleName",
+        message: "Please select the employee's Role:",
+        choices: roles,
+      },
+      {
+        type: "list",
+        name: "managerName",
+        message: "Please select the Employee's Manager: ",
+        choices: manager,
+      },
+    ];
+    inquirer.prompt(questions).then((answers) => {
+      const fName = answers.firstName;
+      const lName = answers.lastName;
+      const rName = answers.roleName;
+      const mName = "%" + answers.managerName + "%";
+      let roleId;
+      let managerId;
+
+      db.query(`SELECT id FROM role WHERE title = ?`, rName, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        roleId = result[0].id;
+
+        db.query(
+          `SELECT id
+            FROM employee
+            WHERE CONCAT(employee.first_name, ' ', employee.last_name) LIKE ?`,
+          mName,
+          (err, result) => {
+            if (err) {
+              console.log(err);
+            }
+            // console.log(result);
+            managerId = result[0].id;
+            db.query(
+              `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                  VALUES (?,?,?,?);`,
+              [fName, lName, roleId, managerId],
+              (err, result) => {
+                if (err) {
+                  console.log(err);
+                }
+                console.log(`${fName} ${lName} has been added.`);
+                repeatCheck();
+              }
+            );
+          }
+        );
+      });
+    });
+  }, 250);
+}
+
+//allows you to update an employee's role
+function updateEmployee() {
+  const roles = getRoles();
+  const employees = getEmployees();
+
+  setTimeout(() => {
+    const questions = [
+      {
+        type: "list",
+        name: "employeeName",
+        message: "Please select the Employee to update:",
+        choices: employees,
+      },
+      {
+        type: "list",
+        name: "roleName",
+        message: "Please select the Role you would like to assign:",
+        choices: roles,
+      },
+    ];
+    inquirer.prompt(questions).then((answers) => {
+      const longEmpName = answers.employeeName;
+      const eName = "%" + answers.employeeName + "%";
+      const role = answers.roleName;
+      let roleId;
+      let empId;
+
+      db.query(`SELECT id FROM role WHERE title = ?`, role, (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        roleId = result[0].id;
+
+        db.query(
+          `SELECT id FROM employee 
+          WHERE CONCAT(employee.first_name, ' ', employee.last_name) LIKE ?`,
+          eName,
+          (err, result) => {
+            if (err) {
+              console.log(err);
+            }
+            // console.log(result);
+            empId = result[0].id;
+            // console.log("empID", empId, "roleId", roleId);
+            db.query(
+              `UPDATE employee SET role_id = ? WHERE id = ?`,
+              [roleId, empId],
+              (err, result) => {
+                if (err) {
+                  console.log(err);
+                }
+                console.log(`${longEmpName} has been updated to be a ${role}.`);
+                repeatCheck();
+              }
+            );
+          }
+        );
+      });
+    });
+  }, 100);
+}
+
+//central function of the app - goes to the menu.
 function menu() {
   inquirer
     .prompt({
@@ -210,6 +386,7 @@ function menu() {
     });
 }
 
+//the prompt to exit or continue after each option is used. on a little delay to ensure the function runs fully before displaying this option.
 function repeatCheck() {
   setTimeout(() => {
     inquirer
